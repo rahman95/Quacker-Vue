9a2ce0ccb0687f41a54c3bd22bbd6abf5c8cd0fa
Rahman Younus <rahman_95@live.co.uk>
2018-07-05 23:21:57 +0100

    added signup component

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VeeValidate from 'vee-validate';
+import axios from 'axios';
 import App from './App';
 import router from './router';
 
+Vue.use(VeeValidate);
 Vue.config.productionTip = false;
+window.axios = axios;
+axios.defaults.baseURL = 'http://127.0.0.1:3333';
 
 /* eslint-disable no-new */
 new Vue({
